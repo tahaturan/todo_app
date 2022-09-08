abe756488535e7792f044c4ef49212eeae231a27
Taha Turan <tahaaturann16@gmail.com>
2022-09-08 07:23:29 +0300

    bottomModalSheet olusturuldu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,10 +6,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('AnaSayfa'),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              _showAddTaskBottomSheet(context);
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: Container(),
+    );
+  }
+
+  void _showAddTaskBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: const ListTile(
+            title: TextField(
+              style: TextStyle(fontSize: 20),
+              decoration: InputDecoration(
+                hintText: "Gorev",
+                border: InputBorder.none,
+                icon: Icon(Icons.edit),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
